980e8dcfd27c7ba8462214a861b616be49daf72c
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-10-18 17:20:07 +0300

    Local

---
+++ NET.W.2017.Arkhipenko.1/QuickSort/SortArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public static class SortArray
+    {
+        public static void QuickSorting(int[] elements, int left, int right)
+        {
+            int i = left, j = right;
+            IComparable pivot = elements[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (elements[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (elements[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    // Swap
+                    int tmp = elements[i];
+                    elements[i] = elements[j];
+                    elements[j] = tmp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            // Recursive calls
+            if (left < j)
+            {
+                QuickSorting(elements, left, j);
+            }
+
+            if (i < right)
+            {
+                QuickSorting(elements, i, right);
+            }
+        }
+
+
+    }
+}
---
+++ NET.W.2017.Arkhipenko.1/Test/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QuickSort.SortArray;
+using static SortMerge.Sortmerge;
+
+namespace Test
+{
+    class Program
+    {
+        static void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rd = new Random();
+            for (int i = 0; i < array.Length; ++i)
+            {
+                array[i] = rd.Next(1, 20);
+            }
+            Console.WriteLine("Массив до сортировки: ");
+            Print(array);
+
+
+            Console.WriteLine("\nQuickSorting : ");
+            QuickSorting(array, 0, array.Length - 1);
+
+            Print(array);
+
+            int[] array1 = new int[10];
+       
+            for (int i = 0; i < array1.Length; ++i)
+            {
+                array1[i] = rd.Next(1,20);
+            }
+            Console.WriteLine("\nМассив до сортировки: ");
+            Print(array1);
+
+            Console.WriteLine("\nСортровка слиянием");
+
+            SortMergeMethod(array1, 0, array1.Length - 1);
+            Print(array1);
+            Console.ReadKey();
+        }
+
+
+        
+    }
+}
