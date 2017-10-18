bcc7b7f234919ba0ed1cd20b2cbb871d275b9356
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-10-18 17:50:36 +0300

    Dop

---
+++ NET.W.2017.Arkhipenko.1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University;
+
+namespace ConsoleStudent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Students("Arkhipenko", "RFiCT");
+            Console.WriteLine(student.ToString());
+            Console.ReadKey();
+        }
+    }
+}
--- NET.W.2017.Arkhipenko.1/University/Students.cs
+++ NET.W.2017.Arkhipenko.1/University/Students.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace University
 {
-    public class Class1
+    public  class Students
     {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+
+        public Students(string name, string faculty)
+        {
+            this.Name = name;
+            this.Faculty = faculty;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Студент {0} учится на факультете : {1}", Name, Faculty);
+        }
     }
 }
